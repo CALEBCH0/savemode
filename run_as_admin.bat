94d36053aec5c2f46497cc81588ec0a4a46e7301
CALEBCH0 <gmcho03@gmail.com>
2025-08-07 13:35:24 +0900

    fix path issue for admin script

--- run_as_admin.bat
+++ run_as_admin.bat
@@ -1,9 +1,13 @@
 @echo off
+:: Get the directory where this batch file is located
+cd /d "%~dp0"
+
 :: Check for admin rights
 net session >nul 2>&1
 if %errorLevel% == 0 (
     echo Running with admin privileges...
-    python battery_saver.py
+    echo Working directory: %cd%
+    python "%~dp0battery_saver.py"
 ) else (
     echo Requesting admin privileges...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
